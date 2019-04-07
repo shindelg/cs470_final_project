13ddd49e326a630ef73cd9853299f6fbdb0fec86
Unknown <frankie.dennehy@gmail.com>
2019-04-07 19:50:59 -0400

    Added Sqlite files
    I'm still working on loading data into db

---
+++ DropTable.py
@@ -0,0 +1,22 @@
+# importing module
+import sqlite3
+
+#This file it used to drop the PublicCompany file from the
+#Temp_ForestTrends database
+
+# connecting to the database
+connection = sqlite3.connect("Temp_ForestTrends.db")
+
+# cursor
+crsr = connection.cursor()
+
+# SQL command to Drop Tables
+crsr.execute(" Drop table if exists PublicCompany; ")
+
+
+# To save the changes in the files. Never skip this.
+# If we skip this, nothing will be saved in the database.
+connection.commit()
+
+# close the connection
+connection.close()

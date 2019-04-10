80ec301494ee4c7b4ef7a7b4677030cb1669661a
Unknown <frankie.dennehy@gmail.com>
2019-04-10 15:23:01 -0400

    deleted comments

--- BuildSqliteBD.py
+++ BuildSqliteBD.py
@@ -1,5 +1,5 @@
 # importing module
-import sqlite3, csv, pandas
+import sqlite3, csv
 #import "./publicComp.csv"
 
 # connecting to the database
@@ -9,64 +9,7 @@ connection = sqlite3.connect("Temp_ForestTrends.db")
 crsr = connection.cursor()
 
 # SQL command to create a table in the database This will be used to Creat
-# ForestTrends original csv in sqlite
-# sql_command = """CREATE TABLE PublicCompany (
-# CompanyId INTEGER PRIMARY KEY,
-# CompanySiteID INTEGER,
-# CompanyName VARCHAR(50),
-# PostToWebsite VARCHAR(50),
-# Visible VARCHAR(10),
-# Researcher VARCHAR(50),
-# Cohort INTEGER,
-# DateLastUpdated VARCHAR(50),
-# RefreshSchedule VARCHAR(50),
-# NextRefresh VARCHAR(50),
-# RelevantParentCompany VARCHAR(50),
-# RelevantSubsidiaryCompanies VARCHAR(50),
-# AllAliases VARCHAR(50),
-# FirstEightAliases VARCHAR(50),
-# AllOtherAliases VARCHAR(50),
-# CompanyWebsite VARCHAR(50),
-# Majorcompanybrands VARCHAR(50),
-# HeadquarterCountry VARCHAR(50),
-# PublicOrPrivate VARCHAR(50),
-# TickerSymbol VARCHAR(20),
-# Sector VARCHAR(50),
-# Industry VARCHAR(50),
-# MarketCapitalizationDisclosed VARCHAR(50),
-# MarketCapitalization INTEGER,
-# AnnualRevenueDisclosed VARCHAR(50),
-# AnnualRevenue INTEGER,
-# FinancesCurrency VARCHAR(50),
-# MarketCapitalizationInUsd INTEGER,
-# AnnualRevenueInUsd INTEGER,
-# FinancesCurrentAsOf VARCHAR(50),
-# TwitterHandle VARCHAR(50),
-# SustainabilityReporting VARCHAR(50),
-# SustainabilityCommodityReporting VARCHAR(50),
-# CommodityReportedon VARCHAR(50),
-# SustainabilityReportingYear VARCHAR(50),
-# AcknowledgeDeforestationIssue VARCHAR(50),
-# AcknowlDeforestationwithCommod VARCHAR(50),
-# CDP2015_voluntaryoffsetbuyer VARCHAR(50),
-# PromiseFuturDeforestationCommit VARCHAR(50),
-# CommodityTypeofFutureCommit VARCHAR(50),
-# GeneralCommitment VARCHAR(50),
-# SoyCommitment VARCHAR(50),
-# PalmCommitment VARCHAR(50),
-# TimberPulpCommitment VARCHAR(50),
-# CattleCommitment VARCHAR(50),
-# TotalCommitments INTEGER,
-# DormantCommPresent VARCHAR(50),
-# NumberOfAssessments INTEGER,
-# NumberOfRelatedActivities INTEGER,
-# CreatedAt VARCHAR(50),
-# ModifiedAt VARCHAR(50),
-# VoluntaryOffsets VARCHAR(50),
-# Researcher_Other VARCHAR(50),
-# UpdatedNotes VARCHAR(50));"""
-
-
+# ForestTrends skimmed csv csv in sqlite
 sql_command = """CREATE TABLE PublicCompany (
 CompanyId INTEGER PRIMARY KEY,
 CompanyName VARCHAR(100),

f282e687693c25c6a612989b68bb9603496a82f4
Unknown <frankie.dennehy@gmail.com>
2019-04-15 18:18:53 -0400

    Got rid of commented stuff

--- FetchCommand.py
+++ FetchCommand.py
@@ -11,14 +11,10 @@ crsr = connection.cursor()
 
 # execute the command to fetch all the data from the table emp
 #crsr.execute("SELECT * FROM PublicCompany")
-crsr.execute("SELECT * FROM NewData")
+#crsr.execute("SELECT * FROM NewData")
 
 
 # Function is to compare two tables within sqlite database
-#crsr.execute("SELECT * FROM NewData EXCEPT SELECT * FROM FTData")
-#crsr.execute("SELECT * FROM NewData INNER JOIN FTData WHERE NewData.CompanyId = FTData.CompanyId")
-#crsr.execute("SELECT * FROM NewData LEFT OUTER JOIN FTData WHERE NewData.CompanyId = FTData.CompanyId")
-
 QueryOne = """SELECT * FROM NewData
 JOIN FTData
 WHERE NewData.CompanyId = FTData.CompanyId
@@ -26,13 +22,8 @@ AND (NewData.MarketCapitalizationInUsd != FTData.MarketCapitalizationInUsd
 OR NewData.AnnualRevenueInUsd != FTData.AnnualRevenueInUsd)
 ;"""
 
-
-
-#OR NewData.AnnualRevenueInUsd != FTData.AnnualRevenueInUsd
-#crsr.execute("SELECT * FROM NewData JOIN FTData WHERE NewData.CompanyId = FTData.CompanyId AND NewData.MarketCapitalizationInUsd != FTData.MarketCapitalizationInUsd")
-
 crsr.execute(QueryOne)
-# crsr.execute(QueryTwo)
+
 
 
 # store all the fetched data in the ans variable

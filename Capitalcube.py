28e47a6ccfd773b045d7816cb7f18ebef7c8eb99
Eunkyung Park <43216860+haileyekpark@users.noreply.github.com>
2019-04-16 01:03:56 -0400

    Fix url response, peer column successfully works

--- Capitalcube.py
+++ Capitalcube.py
@@ -6,87 +6,83 @@ import pandas as pd
 import numpy as np
 from pandas.io.json import json_normalize
 from collections import OrderedDict
-
 # Auth
 s = requests.Session()
 s.auth = ('user', 'pw')
 
 def retrieve_data(tickerCSV):
-  # df = pd.read_csv(tickerCSV, index_col=False) # na_values=['NA']
-  df = pd.read_csv(tickerCSV) # na_values=['NA']
-  totalRow = df.shape[0] #49
+
+  df = pd.read_csv(tickerCSV)
+  totalRow = df.shape[0] 
   print("Total ", totalRow, "company information requested")
   
-  columns = ['TickerSymbol', 'Name', 'HeadquarterCountry', 'PublicOrPrivate', 'Sector', 
-  'Industry', 'MarketCapitalization', 'AnnualRevenue', 'FinancesCurrency', 'Exchange', 
-  'MarketCapitalizationInUsd', 'AnnualRevenueInUsd', 'FinancesCurrentAsOf', 'Profile', 'LastUpdated']
+  # columns = ['TickerSymbol', 'Name', 'HeadquarterCountry', 'PublicOrPrivate', 'Sector', 
+  # 'Industry', 'MarketCapitalization', 'AnnualRevenue', 'FinancesCurrency', 'Exchange', 
+  # 'MarketCapitalizationInUsd', 'AnnualRevenueInUsd', 'FinancesCurrentAsOf', 'Profile', 'LastUpdated']
   
-  successDataset = pd.DataFrame(columns=columns)
+  successDataset = pd.DataFrame()
   failedDataset = pd.DataFrame()
   success = []
   failed = []
 
   for index, name in df.itertuples():  
+    print(name)
+    
     if "-" not in name:
       print("Incorrect format")
       print("Needs form: <ticker symbol>-<country code>")
-      return
-
-    # Get url response
-    urls = ["https://api.capitalcube.com/companies/" +name, "https://api.capitalcube.com/companies/" +name +"/peers"]
-    # url = "https://api.capitalcube.com/companies/" +name
-    # resp = requests.get(url)
-
-    for url in urls:
       
-      resp = requests.get(url)
-      
-      if resp.ok:
-        print(resp)
+      if name == 'Subsidiary':
+        continue
+      else:
+        failed.append({'FailedTicker':name})
+        continue
 
+    # Request for company information
+    url1 = "https://api.capitalcube.com/companies/" +name 
+    resp1 = requests.get(url1)
+      
+    if resp1.ok:
+      print(resp1)
+
+      try:
+        data = resp1.json()
+      except json.decoder.JSONDecodeError:
+        print("Invalid ticker symbol: " +name)
+        print("Enter valid NASDQ or exchange ticker")
+        failed.append({'FailedTicker':name})
+        continue
+
+      # Request for company peers
+      url2 = "https://api.capitalcube.com/companies/" +name +"/peers"
+      resp2 = requests.get(url2)
+      
+      if resp2.ok:
+        print("peers:", resp2)
+        
         try:
-          data = resp.json()
+          peersData = resp2.json()
         except json.decoder.JSONDecodeError:
-          print("Invalid ticker symbol: " +name)
-          print("Enter valid NASDQ or exchange ticker")
-          failed.append({'FailedTicker':name})
+          print("Error happened while pulling peers list")
           continue
 
         peersList = []
-        if "peers" in url:
-          holder = data['_links']['cn:peer']
-          for peerTicker in holder:
-            for k, v in peerTicker.items():
-              if k == "symbol":
-                peersList.append(v)
-        print(peersList)
+        holder = peersData['_links']['cn:peer']
+        
+        for peerTicker in holder:
+          for k, v in peerTicker.items():
+            if k == "symbol":
+              peersList.append(v)
 
-        # if not peersList:
-        #   data['peers'] = None
-        # else:
         data['peers'] = peersList
         success.append(data)
-        peersList.clear()
-        # print("dinedoen: ", peersList)
-        
+
   failedDataset = pd.DataFrame.from_dict(failed, orient='columns')
   failedDataset.to_csv("FailedTickers.csv", index=False)
   successDataset = pd.DataFrame.from_dict(success, orient='columns')
   successDataset.to_csv("SuccessTickers.csv", index=False)
 
 def main():
-  retrieve_data("inputCSV.csv")
+  retrieve_data("output.csv")
 
 main()
-
-"""
-Example "inputCSV.csv" format:
-
-Tickers
-MMM-US
-AMC-AU
-WRONG-22
-BARN-CH
-BAS-DE
-
-"""

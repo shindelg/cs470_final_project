cacc3b809de61c8a8d34c0f943f2a9ec032019bb
Unknown <frankie.dennehy@gmail.com>
2019-04-15 20:40:03 -0400

    Added Comparison write out to CSV

--- FetchCommand.py
+++ FetchCommand.py
@@ -1,7 +1,7 @@
 # Python code to demonstrate SQL to fetch data.
 
 # importing the module
-import sqlite3
+import sqlite3, csv
 
 # connect withe the myTable database
 connection = sqlite3.connect("Temp_ForestTrends.db")
@@ -15,14 +15,23 @@ crsr = connection.cursor()
 
 
 # Function is to compare two tables within sqlite database
-QueryOne = """SELECT * FROM NewData
-JOIN FTData
+QueryOne = """SELECT NewData.CompanyId, NewData.CompanyName, NewData.TickerSymbol, NewData.FinancesCurrency, NewData.MarketCapitalizationInUsd, NewData.AnnualRevenueInUsd, NewData.FinancesCurrentAsOf, FTData.MarketCapitalizationInUsd, FTData.AnnualRevenueInUsd
+FROM NewData JOIN FTData
 WHERE NewData.CompanyId = FTData.CompanyId
 AND (NewData.MarketCapitalizationInUsd != FTData.MarketCapitalizationInUsd
 OR NewData.AnnualRevenueInUsd != FTData.AnnualRevenueInUsd)
 ;"""
 
 crsr.execute(QueryOne)
+
+# Output to a csv
+fields = ['CompanyId', 'CompanyName', 'TickerSymbol', 'FinancesCurrency', 'Updated MarketCapitalizationInUsd', 'Updated AnnualRevenueInUsd', 'FinancesCurrentAsOf', 'Original MarketCapitalizationInUsd', 'Original AnnualRevenueInUsd',]
+toExport = crsr.execute(QueryOne)
+
+with open('UpdatedCompanyInformation.csv', 'w+') as f:
+    writer = csv.writer(f)
+    writer.writerows([(fields)])
+    writer.writerows(toExport)
 
 
 

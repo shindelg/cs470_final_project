3621708bd9ab9a00ceceb7c8ffefd8f4409d43f9
Unknown <frankie.dennehy@gmail.com>
2019-04-24 14:08:03 -0400

    Deleted some files

--- BuildSqliteBD.py
+++ BuildSqliteBD.py
@@ -23,21 +23,6 @@ AnnualRevenueInUsd INTEGER,
 FinancesCurrentAsOf DATE
 );"""
 
-# Function to create a table of just the ticker symbols, to be standardized
-CreateTickers = """CREATE TABLE Tickers (
-TickerSymbol VARCHAR(20)
-);"""
-
-# # Function to create the table based on new data
-# CreateNewData = """CREATE TABLE NewData (
-# CompanyId INTEGER PRIMARY KEY,
-# CompanyName VARCHAR(100),
-# TickerSymbol VARCHAR(20),
-# FinancesCurrency VARCHAR(20),
-# MarketCapitalizationInUsd INTEGER,
-# AnnualRevenueInUsd INTEGER,
-# FinancesCurrentAsOf DATE
-# );"""
 
 # Create the FTData Table for loading in unfiltered data
 crsr.execute(CreateFT)
@@ -52,30 +37,13 @@ with open('publicComp.csv', 'rt') as fin:
 connection.executemany("INSERT INTO FTData (CompanyId, CompanyName, TickerSymbol, FinancesCurrency,MarketCapitalizationInUsd,AnnualRevenueInUsd, FinancesCurrentAsOf ) VALUES (?,?,?,?,?,?,?);", to_db)
 
 
-# # Create the New Data table to insert new data from Capital Cube
-# crsr.execute(CreateNewData)
+# Output to a csv
+toExport = crsr.execute("SELECT TickerSymbol FROM FTData WHERE TickerSymbol NOT LIKE '';")
 
-# # This loads the data in from the Updated csv into the NewData Table
-# with open('newdata.csv', 'rt') as fin:
-#     dr = csv.DictReader(fin)
-#     to_db = [(i['CompanyId'], i['CompanyName'], i['TickerSymbol'], i['FinancesCurrency'], i['MarketCapitalizationInUsd'], i['AnnualRevenueInUsd'], i['FinancesCurrentAsOf']) for i in dr]
-#
-# # Insert values from the newdata csv into the NewData Table
-# connection.executemany("INSERT INTO NewData (CompanyId, CompanyName, TickerSymbol, FinancesCurrency,MarketCapitalizationInUsd,AnnualRevenueInUsd, FinancesCurrentAsOf ) VALUES (?,?,?,?,?,?,?);", to_db)
-#
-
-# Create the Tickers table for standardizing ticker symbols
-crsr.execute(CreateTickers)
-
-# Filter the ticker symbols to not include blank tickers
-crsr.execute("SELECT TickerSymbol FROM FTData WHERE TickerSymbol NOT LIKE '';")
-
-# Set up the filtered tickers to be added into a new table named Tickers (one column that is just tickers)
-newTickers= crsr.fetchall()
-
-# Insert the filtered tickers into the Tickers table
-for i in newTickers:
-	crsr.execute("INSERT INTO Tickers (TickerSymbol) VALUES (?);", i)
+with open('output.csv', 'w+') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Tickers'])
+    writer.writerows(toExport)
 
 # To save the changes in the files. Never skip this.
 # If we skip this, nothing will be saved in the database.

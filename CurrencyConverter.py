7ba4b7927741a2202bd39dbe8e559591b82853da
Eunkyung Park <43216860+haileyekpark@users.noreply.github.com>
2019-04-23 20:16:01 -0400

    Create csv for currency conversion

---
+++ CurrencyConverter.py
@@ -0,0 +1,20 @@
+import json
+import sys
+import csv
+import pandas as pd
+from forex_python.converter import CurrencyRates
+from datetime import datetime
+
+
+def main():
+  now= datetime.now()
+  print(now.strftime('%Y-%m-%d %H:%M:%S'))
+  
+  c = CurrencyRates()
+  currencyData = c.get_rates('USD') 
+  df = pd.DataFrame.from_dict(currencyData, orient='index')
+  df.to_csv('CurrencyConverter.csv', header=None)
+
+  print("Currency csv created")
+  
+main()
